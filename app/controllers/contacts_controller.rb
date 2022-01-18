8d2fed3ba40a72a8354a6ed0d71572f2120b17cb
dani <dani.zaghian@gmail.com>
2022-01-18 11:51:03 -0800

    adds geocoder gem and logic for address conversion

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,28 @@
 class ContactsController < ApplicationController
 
-  def first_contact
-    contact = Contact.first #single hash of contact data
+  def index
+    contacts = Contact.all #array of contact hashes
+    render json: contacts.as_json
+  end
+
+  def show
+    contact = Contact.find(params[:id]) #single hash of contact data
     render json: contact.as_json
   end
 
-  def all_contacts
-    contacts = Contact.all #array of contact hashes
-    render json: contacts.as_json
+  def update
+    contact = Contact.find(params[:id])
+    if params[:address]
+      coordinates = Geocoder.coordinates(params[:address])
+      contact.latitude = coordinates[0]
+      contact.longitude = coordinates[1]
+    end
+    contact.first_name = params[:first_name] || contact.first_name
+    contact.last_name = params[:last_name] || contact.last_name
+    contact.email = params[:email] || contact.email
+    contact.phone_number = params[:phone_number] || contact.phone_number
+    contact.save
+    render json: contact.as_json
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # get "/first_contact", controller: "contacts", action: "first_contact"
-  get "/first_contact" => "contacts#first_contact"
-  # get "/all_contacts", controller: "contacts", action: "all_contacts"
-  get "/all_contacts" => "contacts#all_contacts"
+  get "/contacts" => "contacts#index"
+  get "/contacts/:id" => "contacts#show"
+  patch "/contacts/:id" => "contacts#update"
 end
